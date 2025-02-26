5b93caead5971bb48cc99f8af0332fb272c2f133
ZharlieW <zhw306498704@gmail.com>
2025-02-26 17:41:26 +0800

    [update] 1.Update request page

--- lib/pages/request/request.dart
+++ lib/pages/request/request.dart
@@ -1,208 +1,91 @@
+import 'package:aegis/utils/widget_tool.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/cupertino.dart';
 
-import '../../utils/aegis_websocket_server.dart';
-import '../../utils/server_nip46_signer.dart';
-// import '../../utils/server_nip46_signer.dart';
+import '../../common/common_image.dart';
 
 class Request extends StatefulWidget {
   @override
-  _RequestState createState() => _RequestState();
+  RequestState createState() => RequestState();
 }
 
-class _RequestState extends State<Request> {
-
-  String socketInfo = '';
-  String socketConnectInfo = '';
-  String httpInfo = '';
+class RequestState extends State<Request> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-        AppBar(
-          backgroundColor: Colors.lightBlue,
-            title: const Text('Hello WorldWorldWorldWorldWorld'),
+      appBar: AppBar(
+        title: Text(
+          'Incoming Request',
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.w400,
+              ),
         ),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: Text('Start the websocket service'),
-                  ),
-                  Column(
-                    children: [
-                      GestureDetector(
-                        onTap: _startWebSocketServer,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.lightBlue,
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          margin: EdgeInsets.only(left: 20),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 8,
-                          ),
-                          child: Text(
-                              'Start websocket'
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap:(){},
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.lightBlue,
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          margin: EdgeInsets.only(top: 15),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 8,
-                          ),
-                          child: Text(
-                              'Stop websocket'
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CommonImage(
+                  iconName: 'aegis_logo.png',
+                  size: 100,
+                ).setPaddingOnly(
+                  top: 24.0,
+                  bottom: 20.0,
+                ),
               ),
-            ),
-            Text(
-              socketInfo,
-            ),
-
-            Container(
-              padding: EdgeInsets.only(top: 100),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: Text('Connect the websocket service'),
-                  ),
-                  GestureDetector(
-                    onTap: _startSocketClient,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      margin: EdgeInsets.only(left: 20),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 8,
-                      ),
-                      child: Text(
-                          'connect'
-                      ),
+              Text(
+                'Nothing to approve yet',
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.w400,
                     ),
-                  ),
-                ],
               ),
-            ),
-            Text(
-              socketConnectInfo,
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 100),
-                Container(
-                  child: Text('Start the http service'),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                "Why not explore your favorite nostr app  a bit?",
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.w500,
+                    ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                "Discover all Nostr apps for android at nostrapps.com.",
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.w500,
+                    ),
+              ),
+              FilledButton.tonal(
+                onPressed: () {},
+                style: FilledButton.styleFrom(
+                  backgroundColor: Theme.of(context)
+                      .colorScheme
+                      .secondaryContainer, // 背景色
                 ),
-                GestureDetector(
-                  onTap: _startHttpsService,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    margin: EdgeInsets.only(left: 20),
-                    child: Center(
-                      child: Text('Start'),
+                child: Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Discover',
+                    style:
+                    Theme.of(context).textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-              ],
-            ),
-            Text(
-              httpInfo,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 100),
-                Container(
-                  child: Text('Connect the http service'),
-                ),
-                GestureDetector(
-                  onTap: _connectHttpsService,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    margin: EdgeInsets.only(left: 20),
-                    child: Center(
-                      child: Text('connect'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+              ).setPaddingOnly(top: 20.0),
+            ],
+          ),
         ),
       ),
     );
   }
-
-  void _startSocketClient() async {
-    // AegisWebSocketClient.sharedInstance.connect();
-  }
-
-  void _startWebSocketServer() async {
-    var signer = ServerNIP46Signer();
-    signer.start();
-    // var client = NIP46Client(serverUrl: "ws://127.0.0.1:8081");
-    // await client.connect();
-    // String response = await client.requestSign('{"kind":1,"content":"Hello NIP-46"}');
-    // print("📥 远程签名返回: $response");
-    // await AegisWebSocketServer.sharedInstance.start();
-    // String localIp = await AegisWebSocketServer.getLocalIpAddress();
-    // socketInfo = 'WebSocket server will run at: ws://$localIp:${AegisWebSocketServer.sharedInstance.port}';
-    // setState(() {});
-  }
-
-  void _startHttpsService() async{
-    // AegisHttpServer.sharedInstance.start();
-    // String localIp = await AegisWebSocketServer.getLocalIpAddress();
-    // httpInfo = 'Http server will run at: http://$localIp:${AegisWebSocketServer.sharedInstance.port}';
-    // setState(() {});
-
-  }
-
-  void _connectHttpsService() async {
-    // String url = await AegisWebSocketServer.getLocalIpAddress();
-    // AegisHttpServerClient.sharedInstance
-    //     .sendPostRequest('http://$url:8080/hello', {'aa': 'a'});
-  }
-
-
 }
