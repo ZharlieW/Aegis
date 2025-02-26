135d0102a9a8c0b58edf7e117e4857eb6d36312c
ZharlieW <zhw306498704@gmail.com>
2025-02-26 17:33:48 +0800

    [update] 1. Update settings page ( home )

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:aegis/common/common_image.dart';
+import 'package:aegis/utils/widget_tool.dart';
 import 'package:flutter/material.dart';
 
 class Settings extends StatefulWidget {
@@ -10,48 +11,106 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: GestureDetector(
-          child: Column(
-            children: [
-              _itemWidget(Icons.safety_check,'Safe'),
-              _itemWidget(Icons.key,'Backup key'),
-              _itemWidget(Icons.language,'language'),
-              _itemWidget(Icons.perm_data_setting,'Tor/Orbot setting'),
-              _itemWidget(Icons.admin_panel_settings,'Relays'),
-              _itemWidget(Icons.looks_4,'Log'),
-            ],
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: GestureDetector(
+                child: Column(
+                  children: [
+                    _accountView(),
+                    _itemWidget(
+                      iconName: 'backup_icon.png',
+                      content: 'Backup Keys',
+                    ),
+                    _itemWidget(
+                      iconName: 'relays_icon.png',
+                      content: 'Relays',
+                    ),
+                    _itemWidget(
+                      iconName: 'policy_icon.png',
+                      content: 'Sign policy',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget _itemWidget(IconData iconData, String content){
+  Widget _accountView() {
     return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: 30,
-          vertical: 16
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 18),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surfaceContainer,
+        borderRadius: BorderRadius.circular(8),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(
-            iconData,
-            size: 30,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Aegis Account',
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                      fontWeight: FontWeight.w500,
+                    ),
+              ),
+              Text(
+                'npub1smn:2syewl0f',
+                style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                      fontWeight: FontWeight.w500,
+                    ),
+              ),
+            ],
           ),
-          SizedBox(
-            width: 10,
+          Row(
+            children: [
+              Container(
+                width: 48,
+                height: 48,
+                child: Center(
+                  child: CommonImage(
+                    iconName: 'copy_Icon.png',
+                    size: 24,
+                  ),
+                ),
+              ),
+              Container(
+                width: 48,
+                height: 48,
+                child: Center(
+                  child: CommonImage(
+                    iconName: 'logout_icon.png',
+                    size: 24,
+                  ),
+                ),
+              ),
+            ],
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _itemWidget({required String iconName, required String content}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+      child: Row(
+        children: [
+          CommonImage(iconName: iconName, size: 24).setPaddingOnly(right: 12.0),
           Text(
-              content,
-              style:TextStyle(
-                fontSize: 24
-              )
-          )
+            content,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
         ],
       ),
     );
--- lib/utils/account.dart
+++ lib/utils/account.dart
@@ -1,15 +1,36 @@
 import 'dart:async';
-import 'dart:convert';
 import '../nostr/keychain.dart';
 import '../nostr/nips/nip19/nip19.dart';
+
+abstract mixin class AccountObservers {
+  void didLoginSuccess();
+
+  void didSwitchUser();
+
+  void didLogout();
+
+  void didUpdateUserInfo() {}
+}
 
 class Account {
   Account._internal();
   factory Account() => sharedInstance;
   static final Account sharedInstance = Account._internal();
 
+  final List<AccountObservers> _observers = <AccountObservers>[];
+
   String currentPubkey = '';
   String currentPrivkey = '';
+
+  void addObserver(AccountObservers observer) => _observers.add(observer);
+
+  bool removeObserver(AccountObservers observer) => _observers.remove(observer);
+
+  Future<void> loginSuccess() async {
+    for (AccountObservers observer in _observers) {
+      observer.didLoginSuccess();
+    }
+  }
 
   bool isValidPubKey(String pubKey) {
     final pattern = RegExp(r'^[a-fA-F0-9]{64}$');
