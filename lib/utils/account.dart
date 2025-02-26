23cd9d1f3fafd4e431c793b2b6e540d6d58861b8
ZharlieW <zhw306498704@gmail.com>
2025-02-26 14:52:18 +0800

    [add] 1.Add account tool

---
+++ lib/utils/account.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:convert';
+import '../nostr/keychain.dart';
+import '../nostr/nips/nip19/nip19.dart';
+
+class Account {
+  Account._internal();
+  factory Account() => sharedInstance;
+  static final Account sharedInstance = Account._internal();
+
+  String currentPubkey = '';
+  String currentPrivkey = '';
+
+  bool isValidPubKey(String pubKey) {
+    final pattern = RegExp(r'^[a-fA-F0-9]{64}$');
+    return pattern.hasMatch(pubKey);
+  }
+
+  static String getPrivateKey(String nsec){
+    return Nip19.decodePrivkey(nsec);
+  }
+
+  static Keychain generateNewKeychain() {
+    return Keychain.generate();
+  }
+
+  static String getPublicKey(String privkey) {
+    return Keychain.getPublicKey(privkey);
+  }
+
+  static bool validateNsec(String nsecBase64) {
+    try {
+      if (nsecBase64.length != 63) {
+        return false;
+      }
+      if (!nsecBase64.startsWith('nsec')) {
+        return false;
+      }
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> logout() async {
+
+  }
+}
