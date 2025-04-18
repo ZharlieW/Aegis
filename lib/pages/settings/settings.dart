a575b35e9860cd5c53d0f3a74a0c1792835f6c82
ZharlieW <zhw306498704@gmail.com>
2025-04-18 16:04:27 +0800

    [update] 1. Update version

--- lib/pages/settings/settings.dart
+++ lib/pages/settings/settings.dart
@@ -59,7 +59,7 @@ class _SettingsState extends State<Settings> with AccountObservers {
                         }),
                         _itemWidget(
                           iconName: 'version_icon.png',
-                          content: 'Version: 0.1.0',
+                          content: 'Version: 0.1.1',
                           onTap: () => {}
                         ),
                     // _itemWidget(
