bcb97579774bf5bdf249cec0dbaf3bd086c175f0
ZharlieW <zhw306498704@gmail.com>
2025-05-15 17:14:50 +0800

    [update] 1. Update AccountManager

--- lib/utils/account_manager.dart
+++ lib/utils/account_manager.dart
@@ -1,4 +1,5 @@
 import 'package:aegis/db/clientAuthDB_isar.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../db/db_isar.dart';
 import '../db/userDB_isar.dart';
@@ -6,62 +7,89 @@ import '../nostr/utils.dart';
 import 'account.dart';
 import 'local_storage.dart';
 
+abstract mixin class AccountManagerObservers {
+  void didAddApplicationMap();
+  void didRemoveApplicationMap();
+}
+
 class AccountManager {
+  AccountManager._internal();
+  factory AccountManager() => sharedInstance;
+  static final AccountManager sharedInstance = AccountManager._internal();
+
   static const String _accountPrefix = 'aegis_account_map';
+
+  final Map<String, ValueNotifier<ClientAuthDBISAR>> applicationMap = {};
+  final Map<String, UserDBISAR> accountMap = {};
+  final List<AccountManagerObservers> _observers = <AccountManagerObservers>[];
+
+  void addObserver(AccountManagerObservers observer) => _observers.add(observer);
+  bool removeObserver(AccountManagerObservers observer) => _observers.remove(observer);
+
+  void _notify(void Function(AccountManagerObservers) fn) {
+    for (final o in _observers) {
+      fn(o);
+    }
+  }
+
+  void addApplicationMap(ClientAuthDBISAR client, {bool isUpdate = false}) {
+    final key = client.clientPubkey;
+    final notifier = applicationMap[key];
+    if (notifier != null && !isUpdate) return;
+
+    if (notifier == null) {
+      applicationMap[key] = ValueNotifier<ClientAuthDBISAR>(client);
+      _notify((o) => o.didAddApplicationMap());
+    } else {
+      notifier.value = client;
+    }
+  }
+
+  void removeApplicationMap(String clientPubkey) {
+    if (applicationMap.remove(clientPubkey) != null) {
+      _notify((o) => o.didRemoveApplicationMap());
+    }
+  }
 
 
   static Future<Map<String, UserDBISAR>> getAllAccount() async {
-    final rawMap = await LocalStorage.get(_accountPrefix) ?? {};
-
-    final castedMap = Map<String, dynamic>.from(rawMap);
-
-    return Map<String, UserDBISAR>.fromEntries(
-      castedMap.entries.map(
-            (e) => MapEntry(
-          e.key,
-          UserDBISAR.fromJson(e.value),
-        ),
-      ),
-    );
+    final raw = await LocalStorage.get(_accountPrefix) as Map<String, dynamic>? ?? {};
+    return raw.map((k, v) => MapEntry(k, UserDBISAR.fromJson(v)));
   }
 
-  static Future<void> saveAccount(UserDBISAR user) async {
-    Map<String, dynamic> rawMap = await LocalStorage.get(_accountPrefix) ?? {};
-    rawMap[user.pubkey] = user.toJson();
-    await LocalStorage.set(_accountPrefix, rawMap);
-
-    Account.sharedInstance.accountMap[user.pubkey] = user;
+  Future<void> saveAccount(UserDBISAR user) async {
+    final raw = await LocalStorage.get(_accountPrefix) as Map<String, dynamic>? ?? {};
+    raw[user.pubkey] = user.toJson();
+    await LocalStorage.set(_accountPrefix, raw);
+    accountMap[user.pubkey] = user;
   }
 
   static Future<void> deleteAccount(String pubkey) async {
-    final allAccount = await getAllAccount();
-    allAccount.remove(pubkey);
-    await LocalStorage.set(
-      _accountPrefix,
-      allAccount.map((k, v) => MapEntry(k, v.toJson())),
-    );
+    final all = await getAllAccount();
+    all.remove(pubkey);
+    await LocalStorage.set(_accountPrefix, all.map((k, v) => MapEntry(k, v.toJson())));
   }
 
-  static Future<void> initAccountList() async {
-    Map<String, UserDBISAR> storedAccounts = await AccountManager.getAllAccount();
-    String? getCurrentPubkey = await LocalStorage.get('pubkey');
-    String? autoPubkey = LocalStorage.get('pubkey');
+  Future<void> initAccountList() async {
+    final storedAccounts = await getAllAccount();
+    final autoPubkey = LocalStorage.get('pubkey');
     for (final entry in storedAccounts.entries) {
       final pubkey = entry.key;
-      if(pubkey == autoPubkey) continue;
+      if (pubkey == autoPubkey) continue;
 
-      final isar = await DBISAR.sharedInstance.open(pubkey);
-
-      final storedUser = await UserDBISAR.searchFromDB(pubkey);
-      if (storedUser != null) {
-        final decryptedPrivkey = Account.sharedInstance.decryptPrivkey(storedUser);
-        storedUser.privkey = bytesToHex(decryptedPrivkey);
-        Account.sharedInstance.accountMap[pubkey] = storedUser;
+      await DBISAR.sharedInstance.open(pubkey);
+      final user = await UserDBISAR.searchFromDB(pubkey);
+      if (user != null) {
+        user.privkey = bytesToHex(Account.sharedInstance.decryptPrivkey(user));
+        accountMap[pubkey] = user;
       }
 
-      final storedClient = await ClientAuthDBISAR.getAllFromDB();
-      storedClient.map((item) => Account.sharedInstance.addApplicationMap(item)).toList();
+      final clients = await ClientAuthDBISAR.getAllFromDB();
+      for (final client in clients) {
+        addApplicationMap(client);
+      }
 
+      await DBISAR.sharedInstance.closeDatabaseFor(pubkey);
     }
   }
 }
