a9f0f171bb67f88011f5b2bcd323e89547810573
ZharlieW <zhw306498704@gmail.com>
2025-06-18 12:16:30 +0800

    deleted old files

--- lib/nostr/signer/local_nostr_signer.dart
+++ lib/nostr/signer/local_nostr_signer.dart
@@ -1,16 +1,10 @@
-import 'dart:typed_data';
-
 import 'package:aegis/utils/account.dart';
 import 'package:aegis/utils/account_manager.dart';
 import 'package:aegis/utils/thread_pool_manager.dart';
 import 'package:aegis/utils/logger.dart';
-import 'package:aegis/utils/lru_cache.dart';
 import 'package:flutter/foundation.dart';
-import 'package:pointycastle/export.dart';
 import '../event.dart';
-import '../nips/nip04/nip04.dart';
 import '../nips/nip04/nip04_native_channel.dart';
-import '../nips/nip44/nip44_v2.dart';
 import '../nips/nip44/nip44_native_channel.dart';
 import 'nostr_signer.dart';
 
@@ -23,27 +17,8 @@ class LocalNostrSigner implements NostrSigner {
   late String publicKey;
 
   final ThreadPoolManager _threadPool = ThreadPoolManager();
-  final LRUCache<String, ECDHBasicAgreement> _agreementCache = 
-      LRUCache(maxSize: 100, defaultTtl: const Duration(minutes: 15));
-  final LRUCache<String, Uint8List> _nip44KeyCache = 
-      LRUCache(maxSize: 200, defaultTtl: const Duration(minutes: 15));
   final NIP44NativeChannel _nativeChannel = NIP44NativeChannel();
   final NIP04NativeChannel _nip04NativeChannel = NIP04NativeChannel();
-
-  Future<Uint8List?> _getNip44ConvKey(
-      String serverPrivate, String clientPubkey) async {
-    final cacheKey = '$serverPrivate|$clientPubkey';
-    var convKey = _nip44KeyCache.get(cacheKey);
-    if (convKey != null) return convKey;
-
-    await _ensureThreadPoolInitialized();
-    convKey = await _threadPool.runOtherTask(() async => NIP44V2.shareSecret(serverPrivate, clientPubkey));
-
-    if (convKey != null) {
-      _nip44KeyCache.put(cacheKey, convKey);
-    }
-    return convKey;
-  }
 
   void init() {
     privateKey = Account.sharedInstance.currentPrivkey;
@@ -59,9 +34,7 @@ class LocalNostrSigner implements NostrSigner {
   }
 
   String? getPublicKey(String clientPubkey) {
-    return AccountManager
-            .sharedInstance.applicationMap[clientPubkey]?.value.pubkey ??
-        publicKey;
+    return AccountManager.sharedInstance.applicationMap[clientPubkey]?.value.pubkey ?? publicKey;
   }
 
   String? getPrivateKey(String clientPubkey) {
@@ -81,21 +54,8 @@ class LocalNostrSigner implements NostrSigner {
     return event;
   }
 
-  ECDHBasicAgreement getAgreement(String clientPubkey) {
-    final priv = getPrivateKey(clientPubkey)!;
-    final key = '$priv|$clientPubkey';
-    
-    final cached = _agreementCache.get(key);
-    if (cached != null) return cached;
-    
-    final agreement = NIP04.getAgreement(priv);
-    _agreementCache.put(key, agreement);
-    return agreement;
-  }
-
   @override
-  Future<String?> nip44Decrypt(
-      String serverPrivate, String ciphertext, String clientPubkey) async {
+  Future<String?> nip44Decrypt(String serverPrivate, String ciphertext, String clientPubkey) async {
     try {
       await _ensureThreadPoolInitialized();
 
@@ -117,8 +77,7 @@ class LocalNostrSigner implements NostrSigner {
   }
 
   @override
-  Future<String?> nip44Encrypt(
-      String serverPrivate, String plaintext, String clientPubkey) async {
+  Future<String?> nip44Encrypt(String serverPrivate, String plaintext, String clientPubkey) async {
     try {
       await _ensureThreadPoolInitialized();
 
@@ -145,8 +104,8 @@ class LocalNostrSigner implements NostrSigner {
       final serverPrivate = getPrivateKey(clientPubkey)!;
 
       try {
-        final nativeResult = await _nip04NativeChannel.nativeNip04Decrypt(
-            ciphertext, serverPrivate, clientPubkey);
+        final nativeResult =
+            await _nip04NativeChannel.nativeNip04Decrypt(ciphertext, serverPrivate, clientPubkey);
         if (nativeResult != null) {
           AegisLogger.crypto('NIP04 decryption', true, true);
           return nativeResult;
@@ -167,8 +126,8 @@ class LocalNostrSigner implements NostrSigner {
       final serverPrivate = getPrivateKey(clientPubkey)!;
 
       try {
-        final nativeResult = await _nip04NativeChannel.nativeNip04Encrypt(
-            plaintext, serverPrivate, clientPubkey);
+        final nativeResult =
+            await _nip04NativeChannel.nativeNip04Encrypt(plaintext, serverPrivate, clientPubkey);
         if (nativeResult != null) {
           AegisLogger.crypto('NIP04 encryption', true, true);
           return nativeResult;
