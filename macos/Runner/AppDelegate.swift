ebc173ab119ef06885ed94be3a176fa5e2025563
ZharlieW <zhw306498704@gmail.com>
2025-07-31 14:44:45 +0800

    chore: update .gitignore to exclude build artifacts with local paths

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -10,4 +10,14 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
+  
+  override func application(_ application: NSApplication, open urls: [URL]) -> Bool {
+    guard let url = urls.first else { return false }
+    
+    let controller = mainFlutterWindow?.contentViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "app.channel.shared.data", binaryMessenger: controller.engine.binaryMessenger)
+    channel.invokeMethod("onSchemeCalled", arguments: url.absoluteString)
+    
+    return true
+  }
 }
