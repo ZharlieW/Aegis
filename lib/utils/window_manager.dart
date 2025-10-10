19ecfd550a5a76dfe6868152f580604877c4c9b5
ZharlieW <zhw306498704@gmail.com>
2025-10-10 21:33:24 +0800

    Add client window management

---
+++ lib/utils/window_manager.dart
@@ -0,0 +1,119 @@
+
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+import 'platform_utils.dart';
+
+
+class OXWindowManager with WindowListener {
+
+  Map<String, String> windowInfo = {};
+
+  Future initWindow() async {
+    if (!PlatformUtils.isDesktop) return;
+
+    await windowManager.ensureInitialized();
+
+    await windowManager.waitUntilReadyToShow(WindowOptions(
+      title: 'Aegis',
+      size: Size(windowWidth, windowHeight),
+      minimumSize: WindowInfoEx.minWindowSize,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      windowButtonVisibility: true,
+    ));
+
+    if (windowPositionX == null && windowPositionY == null) {
+      await windowManager.center();
+    } else {
+      await windowManager.setPosition(Offset(windowPositionX ?? 0.0, windowPositionY ?? 0.0));
+    }
+    await windowManager.setPreventClose(true);
+
+    await windowManager.show();
+
+    addWindowObserver();
+  }
+
+  void addWindowObserver() {
+    windowManager.addListener(this);
+  }
+
+  void onWindowResized() async {
+    final size = await windowManager.getSize();
+    windowWidth = size.width;
+    windowHeight = size.height;
+  }
+
+  void onWindowMoved() async {
+    final position = await windowManager.getPosition();
+    windowPositionX = position.dx;
+    windowPositionY = position.dy;
+  }
+
+  /// Emitted when the window is going to be closed.
+  void onWindowClose() {
+    windowManager.hide();
+  }
+
+  /// Emitted when the window gains focus.
+  void onWindowFocus() {
+    windowManager.show();
+  }
+}
+
+extension WindowInfoEx on OXWindowManager {
+  get windowInfoStoreKey => 'WindowInfoStoreKey';
+  get windowInfoSizeWidthKey => 'WindowInfoSizeWidth';
+  get windowInfoSizeHeightKey => 'WindowInfoSizeHeight';
+  get windowInfoPositionXKey => 'WindowInfoPositionX';
+  get windowInfoPositionYKey => 'WindowInfoPositionY';
+
+  static Size get minWindowSize => Size(430, 600);
+
+  static Size get defaultWindowSize => Size(850, 850);
+
+  double get windowWidth {
+    final value = windowInfo[windowInfoSizeWidthKey];
+    if (value == null || value.isEmpty) return defaultWindowSize.width;
+
+    return double.tryParse(value) ?? defaultWindowSize.width;
+  }
+
+  set windowWidth(double value) {
+    windowInfo[windowInfoSizeWidthKey] = value.toStringAsFixed(2);
+  }
+
+  double get windowHeight {
+    final value = windowInfo[windowInfoSizeHeightKey];
+    if (value == null || value.isEmpty) return defaultWindowSize.height;
+
+    return double.tryParse(value) ?? defaultWindowSize.height;
+  }
+
+  set windowHeight(double value) {
+    windowInfo[windowInfoSizeHeightKey] = value.toStringAsFixed(2);
+  }
+
+  double? get windowPositionX {
+    final value = windowInfo[windowInfoPositionXKey];
+    if (value == null || value.isEmpty) return null;
+
+    return double.tryParse(value);
+  }
+
+  set windowPositionX(double? value) {
+    windowInfo[windowInfoPositionXKey] = value?.toStringAsFixed(2) ?? '';
+  }
+
+  double? get windowPositionY {
+    final value = windowInfo[windowInfoPositionYKey];
+    if (value == null || value.isEmpty) return null;
+
+    return double.tryParse(value);
+  }
+
+  set windowPositionY(double? value) {
+    windowInfo[windowInfoPositionYKey] = value?.toStringAsFixed(2) ?? '';
+  }
+}
+
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -10,7 +10,9 @@
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 #include <isar_flutter_libs/isar_flutter_libs_plugin.h>
+#include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
+#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
@@ -21,6 +23,10 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
   IsarFlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("IsarFlutterLibsPlugin"));
+  ScreenRetrieverWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenRetrieverWindowsPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
